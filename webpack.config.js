a7c9979ce1e758d8283bc969566207d8d372f1d9
Kiril <kirimmnaz@mail.ru>
2026-02-26 04:23:10 +0300

    fix wp

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
-'use strict';
-
-let path = require('path');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/js/script.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
-    watch: true,
-
-    devtool: "source-map",
-
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/css', to: 'css' },
+                { from: 'src/icons', to: 'icons' },
+                { from: 'src/img', to: 'img' },
+                { from: 'src/index.html', to: 'index.html' }
+            ]
+        })
+    ]
 };
